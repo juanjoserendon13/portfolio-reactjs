d82709ce3c45f260d4f6721d0769a0760118738e
Juan Jose Rendon <juanjoserendon13@outlook.com>
2018-04-19 23:06:06 -0500

    update projects

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,129 +1,153 @@
-import React, { Component } from 'react'
-import { HashRouter, Route } from 'react-router-dom'
-import 'normalize-css'
 
-// Componentes locales
-import SideBar from '../SideBar'
-import CardWork from '../CardWork'
-import About from '../About'
-import ContactForm from '../ContactForm'
-import Footer from '../Footer'
-// Styling
-import styles from './styles.css'
+import React, { Component } from 'react';
 // Componentes externos
-import ScrollableAnchor from 'react-scrollable-anchor'
-import MediaQuery from 'react-responsive'
+import ScrollableAnchor from 'react-scrollable-anchor';
+import MediaQuery from 'react-responsive';
+import Favicon from 'react-favicon';
+import 'normalize-css';
+// Componentes locales
+import SideBar from '../SideBar';
+import CardWork from '../CardWork';
+import About from '../About';
+import ContactForm from '../ContactForm';
+import Footer from '../Footer';
+// Styling
+import styles from './styles.css';
 // Load images
-import logo from '../../img/logo_320.svg'
+import logo from '../../img/logo_320.svg';
 // Incevo
-import img1 from '../../img/incevo/incevo1.jpg'
-import img2 from '../../img/incevo/incevo2.jpg'
-import img3 from '../../img/incevo/incevo3.jpg'
+import img1 from '../../img/incevo/incevo1.jpg';
+import img2 from '../../img/incevo/incevo2.jpg';
+import img3 from '../../img/incevo/incevo3.jpg';
+// Followit
+import imgf1 from '../../img/followit/follow1.jpg';
+import imgf2 from '../../img/followit/follow2.jpg';
+// Course
+import imgc from '../../img/course_manage/course1.png';
+// Verbindung
+import imgv1 from '../../img/verbindung/verbindung(1).jpg';
+import imgv2 from '../../img/verbindung/verbindung(2).jpg';
+import imgv3 from '../../img/verbindung/verbindung(3).jpg';
+import imgv4 from '../../img/verbindung/verbindung(4).jpg';
 
-var imgs = [img1,img2,img3]
+const imgs = [img1, img2, img3];
 
-import imgf1 from '../../img/followit/follow1.jpg'
-import imgf2 from '../../img/followit/follow2.jpg'
+const imgs2 = [imgf1, imgf2];
 
-var imgs2 = [imgf1,imgf2]
+const imgs3 = [imgc];
+
+const imgs4 = [imgv1, imgv2, imgv3, imgv4];
 // Global styles
-require('./globalstyles.css')
+require('./globalstyles.css');
 
-class App extends Component {
-
-  render () {
-    return (
-      <div className={styles.grid}>
-        <ScrollableAnchor id={'me'}>
-          <MediaQuery maxWidth={767} className={styles.header}>
+const App = () => (
+  <div className={styles.grid}>
+    <Favicon url="https://raw.githubusercontent.com/juanjoserendon13/portfolio-reactjs/99351f7c1c2e1fbb3265bebd60fd97a95b9fa06f/src/img/logo.svg" />
+    <ScrollableAnchor id="me">
+      <MediaQuery maxWidth={767} className={styles.header}>
 
 
-              <div className={styles.backlog} />
-              <div className={styles.logo}><img src={logo} alt="Brand Juan Jose" /></div>
-              <div className={styles.navi}>
-                <SideBar />
-              </div>
-              <p className={styles.descr}>
-                <a href="#" className={styles.highlight}>Leader,</a>
-                <a href="#" className={styles.highlight}>persistent,</a>
-                <a href="#" className={styles.highlight}>loving web</a>
-              </p>
-          </MediaQuery>
-        </ScrollableAnchor>
+        <div className={styles.backlog} />
+        <div className={styles.logo}><img src={logo} alt="Brand Juan Jose" /></div>
+        <div className={styles.navi}>
+          <SideBar />
+        </div>
+        <p className={styles.descr}>
+          <span className={styles.highlight}>Leader,</span>
+          <span className={styles.highlight}>persistent,</span>
+          <span className={styles.highlight}>loving web</span>
+        </p>
+      </MediaQuery>
+    </ScrollableAnchor>
 
-        <ScrollableAnchor id={'me'}>
-          <MediaQuery minWidth={768} className={styles.header}>
+    <ScrollableAnchor id="me">
+      <MediaQuery minWidth={768} className={styles.header}>
 
-            <div className={styles.navigation}>
-              <div className={styles.logo}><img src={logo} alt="Brand Juan Jose" /></div>
-              <div className={styles.navi}>
-                <a className={styles.under} href='#me'>Me</a>
-                <a className={styles.under} href='#work'>My Work</a>
-                <a className={styles.under} href='#aboutme'>About Me</a>
-                <a className={styles.under} href='#contact'>Contact Me</a>
-              </div>
-            </div>
-            <p className={styles.descr}>
-              <a href="#" className={styles.highlight}>Leader,</a>
-              <a href="#" className={styles.highlight}>persistent,</a>
-              <a href="#" className={styles.highlight}>loving web</a>
-            </p>
-
-          </MediaQuery>
-        </ScrollableAnchor>
-
-
-        <ScrollableAnchor id={'work'}>
-          <div className={styles.content}>
-            <CardWork
-              key='1'
-              projectImg={'https://mir-s3-cdn-cf.behance.net/projects/202/8a006757270341.Y3JvcCw1MzcsNDIwLDQ1Myw1MA.jpg'}
-              title={'Incevo'}
-              desc={'Interactive Design'}
-              img={imgs}
-              behance={'https://www.behance.net/gallery/57270341/Incevo'}
-              github={'https://github.com/juanjoserendon13/Incevo'}
-
-            />
-            <CardWork
-              key='2'
-              projectImg={'https://mir-s3-cdn-cf.behance.net/projects/202/564dfc56766797.Y3JvcCw5ODcsNzczLDExOSwyMjY.jpg'}
-              title={'Follow It'}
-              desc={'Standalone App'}
-              img={imgs2}
-              behance={'https://www.behance.net/gallery/56766797/Follow-It'}
-              github={'https://github.com/juanjoserendon13/Follow_It_app'}
-            />
-            
+        <div className={styles.navigation}>
+          <div className={styles.logo}><img src={logo} alt="Brand Juan Jose" /></div>
+          <div className={styles.navi}>
+            <a className={styles.under} href="#me">Me</a>
+            <a className={styles.under} href="#work">My Work</a>
+            <a className={styles.under} href="#aboutme">About Me</a>
+            <a className={styles.under} href="#contact">Contact Me</a>
           </div>
-        </ScrollableAnchor>
+        </div>
+        <p className={styles.descr}>
+          <span className={styles.highlight}>Leader,</span>
+          <span className={styles.highlight}>persistent,</span>
+          <span className={styles.highlight}>loving web</span>
+        </p>
 
-        {/* <ScrollableAnchor id={'aboutme'}> */}
-        <About className={styles.about} />
-        {/* </ScrollableAnchor> */}
+      </MediaQuery>
+    </ScrollableAnchor>
 
-        {/* <ScrollableAnchor id={'contact'}> */}
-        <ContactForm className={styles.contact} />
-        {/* </ScrollableAnchor> */}
 
-        <Footer />
+    <ScrollableAnchor id="work">
+      <div className={styles.content}>
+        <CardWork
+          key="1"
+          projectImg="https://mir-s3-cdn-cf.behance.net/projects/202/8a006757270341.Y3JvcCw1MzcsNDIwLDQ1Myw1MA.jpg"
+          title="Incevo"
+          desc="Interactive Design"
+          img={imgs}
+          behance="https://www.behance.net/gallery/57270341/Incevo"
+          github="https://github.com/juanjoserendon13/Incevo"
+        />
+        <CardWork
+          key="2"
+          projectImg="https://mir-s3-cdn-cf.behance.net/projects/202/564dfc56766797.Y3JvcCw5ODcsNzczLDExOSwyMjY.jpg"
+          title="Follow It"
+          desc="Standalone App"
+          img={imgs2}
+          behance="https://www.behance.net/gallery/56766797/Follow-It"
+          github="https://github.com/juanjoserendon13/Follow_It_app"
+        />
+        <CardWork
+          key="3"
+          projectImg="https://mir-s3-cdn-cf.behance.net/projects/202/95d4fd56771761.Y3JvcCwxNzkwLDE0MDEsMTI4LDEwMDM.jpg"
+          title="Grades"
+          desc="Web App"
+          img={imgs3}
+          behance="https://www.behance.net/gallery/56771761/Course-Management-Web-App"
+          github="https://github.com/juanjoserendon13/taller3_P_web"
+        />
+        <CardWork
+          key="4"
+          projectImg="https://mir-s3-cdn-cf.behance.net/projects/202/8d7eee64448959.Y3JvcCw5MjEsNzIxLDQ5OCwxNDc.jpg"
+          title="Verbindung"
+          desc="Game"
+          img={imgs4}
+          behance="https://www.behance.net/gallery/64448959/Verbindung-Game"
+          github="https://www.behance.net/gallery/64448959/Verbindung-Game"
+        />
 
-       </div>
-      // <HashRouter>
-      //   <div>
-      //     <Route exact={true} path='/' render={() => (
-      //       <SecUno></SecUno>
-      //     )} />
-      //
-      //     <Route exact={true} path='/miseccion' render={() => (
-      //       <SecUno text='MI PORTAFOLIO SECCION'></SecUno>
-      //     )} />
-      //
-      //   </div>
-      // </HashRouter> */
+      </div>
+    </ScrollableAnchor>
 
-    )
-  }
-}
+    {/* <ScrollableAnchor id={'aboutme'}> */}
+    <About className={styles.about} />
+    {/* </ScrollableAnchor> */}
 
-export default App
+    {/* <ScrollableAnchor id={'contact'}> */}
+    <ContactForm className={styles.contact} />
+    {/* </ScrollableAnchor> */}
+
+    <Footer />
+
+  </div>
+  // <HashRouter>
+  //   <div>
+  //     <Route exact={true} path='/' render={() => (
+  //       <SecUno></SecUno>
+  //     )} />
+  //
+  //     <Route exact={true} path='/miseccion' render={() => (
+  //       <SecUno text='MI PORTAFOLIO SECCION'></SecUno>
+  //     )} />
+  //
+  //   </div>
+  // </HashRouter> */
+
+);
+
+export default App;
--- src/components/CardWork/index.jsx
+++ src/components/CardWork/index.jsx
@@ -1,9 +1,9 @@
-import React, {Component} from 'react'
-import PropTypes from 'prop-types'
-import styles from './styles.css'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styles from './styles.css';
 
-import ReactModal from 'react-modal'
-import {CSSTransition, transit} from 'react-css-transition'
+import ReactModal from 'react-modal';
+import { CSSTransition, transit } from 'react-css-transition';
 
 const propTypes = {
   projectImg: PropTypes.string.isRequired,
@@ -11,37 +11,37 @@ const propTypes = {
   desc: PropTypes.string.isRequired,
   img: PropTypes.array.isRequired,
   behance: PropTypes.string.isRequired,
-  github: PropTypes.string.isRequired
-}
+  github: PropTypes.string.isRequired,
+};
 
 
 class CardWork extends Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       showModal: false,
-      active: false
-    }
+      active: false,
+    };
 
-    this.handleOpenModal = this.handleOpenModal.bind(this)
-    this.handleCloseModal = this.handleCloseModal.bind(this)
+    this.handleOpenModal = this.handleOpenModal.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
   }
-  handleOpenModal (event) {
-    event.preventDefault()
-    this.setState({showModal: true, active: true})
-  }
-
-  handleCloseModal (event) {
-    event.preventDefault()
-    this.setState({showModal: false, active: false})
+  handleOpenModal(event) {
+    event.preventDefault();
+    this.setState({ showModal: true, active: true });
   }
 
-  render () {
-    const {active} = this.state
+  handleCloseModal(event) {
+    event.preventDefault();
+    this.setState({ showModal: false, active: false });
+  }
+
+  render() {
+    const { active } = this.state;
     return (
       <div className={styles.root} >
         <div className={styles.imgcont}>
-          <a href=' ' onClick={this.handleOpenModal}>
+          <a href=" " onClick={this.handleOpenModal}>
             <img className={styles.backimg} src={this.props.projectImg} />
           </a>
           <div className={styles.cont}>
@@ -49,44 +49,50 @@ class CardWork extends Component {
             <p className={styles.subject}>{this.props.desc}</p>
           </div>
         </div>
-        {/* Hidden content*/}
-          <ReactModal
-            isOpen={this.state.showModal}
-            contentLabel="Modal"
+        {/* Hidden content */}
+        <ReactModal
+          isOpen={this.state.showModal}
+          contentLabel="Modal"
             // className={`styles.modal ${active ? styles.animated : ''}`}
-            className={[styles.modal,  active ? styles.animated : ''].join(' ')}
-            overlayClassName={styles.overlay}>
-            {/*Iconos de navegacion*/}
-            <a href=' '
-            onClick={this.handleCloseModal} className={[styles.button, styles.close].join(' ')}>
-              <span className='fa fa-times'></span>
-            </a>
+          className={[styles.modal, active ? styles.animated : ''].join(' ')}
+          overlayClassName={styles.overlay}
+          >
+          {/* Iconos de navegacion */}
+          <a
+            href=" "
+            onClick={this.handleCloseModal}
+            className={[styles.button, styles.close].join(' ')}
+            >
+            <span className="fa fa-times" />
+          </a>
 
-            <a href={this.props.behance}
-            target="_blank" 
-            className={[styles.button, styles.behance].join(' ')}>
-              <span className='fa fa-behance'></span>
-            </a>
+          <a
+            href={this.props.behance}
+            target="_blank"
+            className={[styles.button, styles.behance].join(' ')}
+            >
+            <span className="fa fa-behance" />
+          </a>
 
-            <a href={this.props.github}
-              target="_blank" 
-              className={[styles.button, styles.github].join(' ')}>
-              <span className='fa fa-github fa-2x'></span>
-            </a>
-          {/*Mapeo de imagenes pasadas por props*/}
-          <div className={styles.contPres}>                   
-             {this.props.img.map((im,index) => {
-              return(
-                <img src={im} className={styles.imgProject} key= {index} />
-             )
-            })} 
+          <a
+            href={this.props.github}
+            target="_blank"
+            className={[styles.button, styles.github].join(' ')}
+            >
+            <span className="fa fa-github fa-2x" />
+          </a>
+          {/* Mapeo de imagenes pasadas por props */}
+          <div className={styles.contPres}>
+            {this.props.img.map((im, index) => (
+              <img src={im} className={styles.imgProject} key={index} />
+             ))}
           </div>
-          </ReactModal>
+        </ReactModal>
 
 
       </div>
-    )
+    );
   }
 }
-CardWork.propTypes = propTypes
-export default CardWork
+CardWork.propTypes = propTypes;
+export default CardWork;
